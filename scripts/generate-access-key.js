df89228a350c284716cedfc0ee0755b94954229b
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-07-31 18:39:48 +0200

    refactor: remove redundant access-key output helper

--- scripts/generate-access-key.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const { randomToken, hashAccessKey } = require("../src/security");
-
-const key = randomToken(32);
-process.stdout.write("URL access key (show once): " + key + "\n");
-process.stdout.write("SIRK_ACCESS_KEY_HASH=" + hashAccessKey(key) + "\n");
-
