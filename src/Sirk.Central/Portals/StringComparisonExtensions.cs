3f9772f4dc1284b806ee1b4431028084a551fa9f
Eris92 <43513086+Eris92@users.noreply.github.com>
2026-08-07 22:57:24 +0200

    fix(build): reuse ordinal char string comparison helper

--- src/Sirk.Central/Portals/StringComparisonExtensions.cs
+++ src/Sirk.Central/Portals/StringComparisonExtensions.cs
@@ -12,4 +12,15 @@ internal static class StringComparisonExtensions
             _ => string.Compare(value, 0, character.ToString(), 0, 1, comparisonType) == 0
         };
     }
+
+    public static bool Contains(this string value, char character, StringComparison comparisonType)
+    {
+        return comparisonType switch
+        {
+            StringComparison.Ordinal => value.IndexOf(character) >= 0,
+            StringComparison.OrdinalIgnoreCase => value.IndexOf(char.ToUpperInvariant(character)) >= 0 ||
+                                                  value.IndexOf(char.ToLowerInvariant(character)) >= 0,
+            _ => value.IndexOf(character.ToString(), comparisonType) >= 0
+        };
+    }
 }
